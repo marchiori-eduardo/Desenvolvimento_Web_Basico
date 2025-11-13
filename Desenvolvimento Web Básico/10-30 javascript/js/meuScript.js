4142e5e14e808e61238ce8207fa491e628c74524
Eduardo Marchiori <145299156+marchiori-eduardo@users.noreply.github.com>
2025-11-12 21:08:57 -0300

    updates

---
+++ Desenvolvimento Web Básico/10-30 javascript/js/meuScript.js
@@ -0,0 +1,16 @@
+function ligar() {
+    document.getElementById('lampa').src='img/pic_bulbon.png';
+}
+function desligar() {
+    document.getElementById('lampa').src='img/pic_bulboff.png';
+}
+function onOff() {
+    let imagem = document.getElementById('lampad').src;
+
+    if (imagem.includes('img/pic_bulboff.png') ) {
+        document.getElementById('lampad').src='img/pic_bulbon.png';
+    }
+    else {    
+        document.getElementById('lampad').src='img/pic_bulboff.png';
+    }
+}
